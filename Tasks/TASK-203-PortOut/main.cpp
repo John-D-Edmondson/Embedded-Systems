63d105fa5d9b2b0cf03a7d2bbdbb7b6cc96e0712
John-D-Edmondson <john.edmondson@students.plymouth.ac.uk>
2023-02-10 09:05:38 +0000

    Task 203/204

--- Tasks/TASK-203-PortOut/main.cpp
+++ Tasks/TASK-203-PortOut/main.cpp
@@ -4,18 +4,28 @@
 #define TRAF_GRN1_PIN PC_6
 #define TRAF_YEL1_PIN PC_3
 #define TRAF_RED1_PIN PC_2
-#define LEDMASK 0b0000000001001100
+#define LEDMASKC 0b0000000001001100
+#define LEDMASKB 0b0000000010000001
 // Objects
 //BusOut leds(TRAF_RED1_PIN, TRAF_YEL1_PIN, TRAF_GRN1_PIN);
-PortOut leds(PortC, LEDMASK);
+PortOut ledsC(PortC, LEDMASKC);
+
+PortOut ledsB(PortB, LEDMASKB);
+
+DigitalOut light1(PB_0);
+DigitalOut light2(PB_7);
+DigitalOut light3(PB_14);
+
 
 int main()
 {
+
     while (true) {
-        leds = 0;   //Binary 000
+        //light3 = 0;
+        ledsB = ledsB ^ LEDMASKB;
+        ledsC = ledsC ^ LEDMASKC;  
         wait_us(500000);
-        leds = 0xFF;   //Binary 11111111
-        wait_us(500000);    
+  
     }
 }
 
